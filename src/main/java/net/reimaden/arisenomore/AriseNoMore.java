abcc2f51390d4cd56d00021cf44ec7bea8548cc7
Maxmani <mikael.hannukainen@gmail.com>
2023-01-10 01:07:01 +0200

    Quilt port

---
+++ src/main/java/net/reimaden/arisenomore/AriseNoMore.java
@@ -0,0 +1,32 @@
+package net.reimaden.arisenomore;
+
+import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unused")
+public class AriseNoMore implements ModInitializer {
+
+	public static final String MOD_ID = "arisenomore";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	private static final int CHANCE = 100;
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hey! Wake up!");
+
+		// Kill the player when they wake up and if the condition is true
+		EntitySleepEvents.STOP_SLEEPING.register((entity, sleepingPos) -> {
+			ServerPlayerEntity player = (ServerPlayerEntity) entity;
+			int random = player.getRandom().nextInt(CHANCE);
+
+			if (random == 0 && player.getSleepTimer() >= 100) {
+				player.damage(DamageSource.GENERIC, Float.MAX_VALUE);
+			}
+		});
+	}
+}
